408749158b5b7a8ef33be84192748409ce5b07e8
Xaria Prempeh <84147771+XariaP@users.noreply.github.com>
2023-07-17 16:31:05 -0400

    Update run.sh
    Add loading of backup data

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ cd backend
 source "venv/bin/activate"
 ./manage.py makemigrations
 ./manage.py migrate
+./manage.py loaddata p3.json
 ./manage.py runserver & cd ../frontend/restify/ && npm start && fg
