bc8368bdeb5cd394039544efd80e3b1f17c26526
Xaria Prempeh <84147771+XariaP@users.noreply.github.com>
2023-07-22 14:24:50 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ source "venv/bin/activate"
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata p3.json
-python3 manage.py runserver & cd ../frontend/restify/ && npm start && fg
+python3 manage.py runserver & cd ../frontend/ && npm start && fg
