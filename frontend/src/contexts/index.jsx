213a120b20bb7a3ad0c37eb0b472229ed8577fc1
Xaria Prempeh <84147771+XariaP@users.noreply.github.com>
2023-07-23 17:29:48 -0400

    Add comments to context

--- frontend/src/contexts/index.jsx
+++ frontend/src/contexts/index.jsx
@@ -1,20 +1,24 @@
 import { createContext, useState } from "react";
 
+// Store current page being viewed
 export const PageContext = createContext({
     page: "home",
     setPage: () => {},
 });
 
+// Store the token of the currently logged in user's session
 export const UserContext = createContext({
     token: "",
     setToken: () => {},
 });
 
+// Store the criteria to be matched when searching for a rental
 export const SearchContext = createContext({
     value: "",
     setValue: () => {}
 })
 
+// Store values to be matched during search
 export function useSearchContext() {
     const [ beds, setBeds ] = useState(0);
     const [ baths, setBaths ] = useState(0);
